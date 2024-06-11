ec4e7f054072a1eebf654a6078eb0b9eb0e596c5
Evercley Vieira <everkley_@hotmail.com>
2024-06-11 10:25:40 -0300

    Web Service - Implementação do findAll e findById;

--- src/main/java/com/atelion/webservice/resources/UserResource.java
+++ src/main/java/com/atelion/webservice/resources/UserResource.java
@@ -1,25 +1,40 @@
 package com.atelion.webservice.resources;
 
 import com.atelion.webservice.entities.User;
-import com.atelion.webservice.repositories.UserRepository;
+import com.atelion.webservice.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
+@Tag(name = "get", description = "")
 public class UserResource {
 
+    @Autowired
+    private UserService userService;
 
-    @Tag(name = "get", description = "Retorna uma lista de usuários")
+    @Operation(summary = "get lista de usuários", description = "retorna uma lista com todos os usuários")
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        List<User> users = new ArrayList<>();
-        return null;
+    public ResponseEntity<List<User>> findAll() {
+        List<User> users = userService.findAll();
+        return ResponseEntity.ok().body(users);
+    }
+
+    @Operation(summary = "get usuário por id", description = "retorna um usuário com base no id informado")
+    @GetMapping(value ="/{id}")
+    public ResponseEntity<User> findById(@PathVariable(value = "id", required = true)
+                                             @Parameter(name = "id", description = "Id do usuário",
+                                                     example = "1") Long id){
+        User user = userService.findById(id);
+        return ResponseEntity.ok().body(user);
     }
 }
